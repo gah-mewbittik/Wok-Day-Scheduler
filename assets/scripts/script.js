616688f2addc3c7ff75e1a69999f832844774f7f
Andrew Biron <andrewbiron@andrews-air.home>
2023-12-07 01:17:40 -0500

    completed local storage set up

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-//current time
- 
 
+ // time block for savingEntry function.
+var timeBlockId;
+
+// array for schedule planner
 var scheduleTime = [
   {
     time: '09 AM',
@@ -42,28 +44,18 @@ var scheduleTime = [
     scheduleItem: ''
   }
 ];
+
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
 
-    //
-
-    
+    //Looping through ScheduleTime array
      for(var i = 0; i < scheduleTime.length; i++){
+      //creating div element
       var scheduleBlk_El = document.createElement('div');
       scheduleBlk_El.setAttribute('id', 'hour-' + (i + 9));
       scheduleBlk_El.classList.add('row', 'time-block');
 
+      //adding past, present, and future classes
       if(i + 9 < dayjs().hour()){
         scheduleBlk_El.classList.add('past');
       }else if(i + 9 === dayjs().hour()){
@@ -71,57 +63,66 @@ $(function () {
       }else{
         scheduleBlk_El.classList.add('future');
       }
-
+      //creating div for time
       var time_El = document.createElement('div');
       time_El.classList.add('col-2', 'col-md-1', 'hour', 'text-center', 'py-3');
       time_El.textContent = scheduleTime[i].time;
 
+      //creating textarea
       var txtArea_El = document.createElement('textarea');
       txtArea_El.classList.add('col-8', 'col-md-10', 'description');
       txtArea_El.setAttribute('rows', '3');
       txtArea_El.value = scheduleTime[i].scheduleItem;
 
+      //create save button
       saveButton_El = document.createElement('button');
       saveButton_El.classList.add('btn', 'saveBtn', 'col-2', 'col-md-1');
       saveButton_El.setAttribute('aria-label', 'save');
 
+      //create the icon for save button
       var icon_El = document.createElement('i');
       icon_El.classList.add('fas', 'fa-save');
       icon_El.setAttribute('aria-hidden', 'true');
 
+      //appending created elements
       saveButton_El.appendChild(icon_El);
 
       scheduleBlk_El.appendChild(time_El);
       scheduleBlk_El.appendChild(txtArea_El);
       scheduleBlk_El.appendChild(saveButton_El);
 
+      //calling highlightedTimeSlot
+      var highlightedClass = highlightedTimeSlot(i);
+      scheduleBlk_El.classList.add(highlightedClass);
+
       document.querySelector('.container-lg').appendChild(scheduleBlk_El);
 
     }
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    
    
-  
-   
-    // TODO: Add code to display the current date in the header of the page.
-     
+    //calling current date function
     currentDate();
+
+    //highlightedTimeSlot();
+   
+    //calling saveEntry function
     saveEntry();
+
+    // calling the loadEntries function
+    loadEntries();
   });
 
   //Get current date function for header
   function currentDate(){
-    var currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A'); 
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY H:mm A'); 
     $('#currentDay').text(currentDate);
   }
   
+  //Saving the entries
   function saveEntry(){
     $('.saveBtn').on('click', function () {
       // Get the id of the containing time-block
-      var timeBlockId = $(this).closest('.time-block').attr('id');
-  
+      timeBlockId = $(this).closest('.time-block').attr('id');
+      
       // Get the user input from the textarea
       var userInput = $(this).prev('.description').val();
   
@@ -130,12 +131,33 @@ $(function () {
     });
   }
   
+  //loading the entries
+  function loadEntries(){
+    for (var i = 0; i < scheduleTime.length; i++) {
+      var timeBlockId = 'hour-' + (i + 9);
+      var savedData = localStorage.getItem(timeBlockId);
 
-  function highlightedTimeSlot(time){
-    var currentTime = dayjs.format('h:mm A');
-    var userEntry = 0; // fix this  (e.g. var planEntry = moment(time, "H A"); )
+      if (savedData) {
+        scheduleTime[i].scheduleItem = savedData;
+        var textarea = document.querySelector('#' + timeBlockId + ' .description');
+        textarea.value = savedData;
+      }
+    }
 
-    ///ideas how to code this!
+  }
+
+  function highlightedTimeSlot(index){
+    var currentTime = dayjs().format('H:mm A');
+    var time = dayjs(scheduleTime[index].time, 'H A');
+   
+    if(dayjs(currentTime, 'H:mm A').isBefore(time)){
+      return 'future';
+    }else if(dayjs(currentTime, 'H:mm A').isAfter(time)){
+      return 'past';
+    }else{
+      return 'present';
+    }
+
   }
 
 
